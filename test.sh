e54e55501e1beab75e549b0298f3a571d1e4e83d
Mac Ryan <quasipedia@gmail.com>
2014-12-19 23:04:40 +0100

    Stub of testing infrastructure

---
+++ test.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env sh
+python -m nose --rednose --verbose --with-coverage --cover-erase --cover-package simpleactors
---
+++ test/tests.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+'''
+Test suite for Simple Actors
+'''
+
+import unittest
+
+import simpleactors as sa
+
+
+class TestWhatever(unittest.TestCase):
+
+    def test_foo(self):
+        self.assertFalse(True)
