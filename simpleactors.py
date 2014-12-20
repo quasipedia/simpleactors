be8f7b36893241ff07393791f703a94d1069b659
Mac Ryan <quasipedia@gmail.com>
2014-12-20 16:40:11 +0100

    Refactoring, more tests

--- simpleactors.py
+++ simpleactors.py
@@ -15,13 +15,12 @@ global_event_queue = deque()
 global_callbacks = defaultdict(set)
 
 # Messages that can be emitted by Actors
-ATTACH = object()
-DETACH = object()
-REMOVE = object()
-HALT = object()
+KILL = object()  # Kill an actor
+HALT = object()  # Hald the main loop
+
 # Messages that can be emitted by Director
-INTIATE = object()
-FINISH = object()
+INITIATE = object()  # First action when the loop starts
+FINISH = object()  # Las action before the loop ends
 
 
 def on(message):
@@ -41,18 +40,30 @@ class Actor:
 
     def __init__(self):
         global_actors.add(self)
-        self._is_attached = False
-        self._attach()
+        self.__plugged = False
+        self.plug()
 
-    def _attach(self):
-        '''Edd the actor's methods to the callback registry.'''
-        if self._is_attached:
+    def plug(self):
+        '''Add the actor's methods to the callback registry.'''
+        if self.__plugged:
             return
         for _, method in inspect.getmembers(self, predicate=inspect.ismethod):
             if hasattr(method, '_callback_messages'):
                 for message in method._callback_messages:
                     global_callbacks[message].add(method)
-        self._is_attached = True
+        self.__plugged = True
+
+    def unplug(self):
+        '''Remove the actor's methods from the callback registry.'''
+        if not self.__plugged:
+            return
+        for callbacks in global_callbacks.values():
+            callbacks.discard(self)
+        self.__plugged = False
+
+    @property
+    def is_plugged(self):
+        return self.__plugged
 
     def emit(self, message, *args, **kwargs):
         '''Emit an event.'''
@@ -70,29 +81,24 @@ class Director(Actor):
 
     def run(self):
         '''Run until there are no events to be processed.'''
-        self.emit(INTIATE)
+        self.emit(INITIATE)
         while global_event_queue:
             self.process_event(global_event_queue.popleft())
 
-    @on(ATTACH)
-    def attach(self, event, emitter, *args, **kwargs):
-        emitter._attach()
+    @property
+    def actors(self):
+        ret = global_actors.copy()
+        ret.remove(self)
+        return ret
 
-    @on(DETACH)
-    def detach(self, event, emitter, *args, **kwargs):
-        '''Remove the actor's methods from the callback registry.'''
-        for message, callbacks in global_callbacks.items():
-            if message is not ATTACH:
-                callbacks.discard(emitter)
-
-    @on(REMOVE)
+    @on(KILL)
     def remove(self, event, emitter, *args, **kwargs):
-        self.detach(DETACH, emitter)
-        global_callbacks[ATTACH].remove(emitter)
-        global_actors.remove(emitter)
+        target = args[0]
+        target.unplug()
+        global_actors.remove(target)
 
     @on(HALT)
     def halt(self, message, emitter, *args, **kwargs):
         '''Halt the execution of the loop.'''
         self.process_event((FINISH, self, (), {}))
-        exit()
+        global_event_queue.clear()
--- test/tests.py
+++ test/tests.py
@@ -4,19 +4,20 @@ Test suite for Simple Actors
 '''
 
 import unittest
+from unittest import mock
 from collections import deque, defaultdict
 
 import simpleactors as sa
 
 
-class DummyActorOne(sa.Actor):
+class ActorSingle(sa.Actor):
 
-    @sa.on('foo')
+    @sa.on('echo')
     def foo_callback(self, message, emitter, *args, **kwargs):
-        pass
+        kwargs['ret_value'] = 'spam'
 
 
-class DummyActorTwo(sa.Actor):
+class ActorDouble(sa.Actor):
 
     @sa.on('foo')
     @sa.on('bar')
@@ -44,13 +45,13 @@ class TestActor(BaseTest):
 
     def test_add_to_callbacks(self):
         '''Intantiating an actor add its callback to the global registry.'''
-        actor = DummyActorOne()
-        expected = {'foo': set([actor.foo_callback])}
+        actor = ActorSingle()
+        expected = {'echo': set([actor.foo_callback])}
         self.assertEqual(expected, sa.global_callbacks)
 
     def test_double_decorated(self):
         '''It is possible to double-decorate a callback.'''
-        actor = DummyActorTwo()
+        actor = ActorDouble()
         expected = {'foo': set([actor.double_callback]),
                     'bar': set([actor.double_callback])}
         self.assertEqual(expected, sa.global_callbacks)
@@ -58,13 +59,48 @@ class TestActor(BaseTest):
     def test_emit_appends(self):
         '''Emitting an event will append it to the global deque.'''
         sa.global_event_queue.append('scrap-me')
-        actor = DummyActorOne()
+        actor = ActorSingle()
         actor.emit('spam')
         self.assertTrue('spam', sa.global_event_queue[1])
 
     def test_emit_payload(self):
         '''Emitting an event append the correct payload.'''
-        actor = DummyActorOne()
+        actor = ActorSingle()
         actor.emit('spam', 42, foo='bar')
         expected = ('spam', self.test_emit_payload, (42, ), {'foo': 'bar'})
         self.assertTrue(expected, sa.global_event_queue[0])
+
+
+class TestDirector(BaseTest):
+
+    '''Test suite for the Director class.'''
+
+    def test_run_send_initiate_event(self):
+        '''Running the Dirctor emit the INITIATE signal.'''
+        director = sa.Director()
+        with mock.patch.object(director, 'emit') as mock_emit:
+            director.run()
+            mock_emit.assert_called_once_with(sa.INITIATE)
+
+    def test_halt(self):
+        '''Halting the directory process FINISH and reset the event queue.'''
+        director = sa.Director()
+        sa.global_event_queue.append('foo')
+        with mock.patch.object(director, 'process_event') as mock_process:
+            director.halt(sa.HALT, self)
+            self.assertEqual([], list(sa.global_event_queue))
+            mock_process.assert_called_once_with((sa.FINISH, director, (), {}))
+
+
+class TestMessageHandling(BaseTest):
+
+    '''Test that messages are handled properly.'''
+
+    def test_callback_triggered(self):
+        '''A callback is triggered on emitting the bound signal'''
+        actor = ActorSingle()
+        director = sa.Director()
+        test_dict = {'ret_value': None}
+        actor.emit('echo', self, (), test_dict)
+        director.run()
+        self.assertEqual('spam', test_dict['ret_value'])
